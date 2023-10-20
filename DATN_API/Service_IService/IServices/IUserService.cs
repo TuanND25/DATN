4fffa6cdf179af2afb4e33c1fd71a4065d9aa2d5
nguyenkien.contact.working@gmail.com <nguyenkien.contact.working@gmail.com>
2023-10-21 01:01:59 +0700

    manager user

--- DATN_API/Service_IService/IServices/IUserService.cs
+++ DATN_API/Service_IService/IServices/IUserService.cs
@@ -1,10 +1,13 @@
 ﻿using DATN_Shared.Models;
+using DATN_Shared.ViewModel;
 
 namespace DATN_API.Service_IService.IServices
 {
 	public interface IUserService
 	{
-		public Task<User> UpdateUser(User user);
+		public Task<User> UpdateUser(User_VM user);
+		
 
+		public Task<List<User>> GetUserByUserName(string username);
 	}
 }
---
+++ DATN_Client/Areas/Admin/Components/Address.razor.cs
@@ -0,0 +1,17 @@
+﻿using DATN_Shared.Models;
+using DATN_Shared.ViewModel;
+using Microsoft.AspNetCore.Components;
+
+namespace DATN_Client.Areas.Admin.Components
+{
+    public partial class Address
+    {
+        [Inject] NavigationManager navigationManager { get; set; }
+        HttpClient httpClient = new HttpClient();
+        public List<AddressShip_VM> addressShip_s  = new List<AddressShip_VM>();   
+        protected override async Task OnInitializedAsync()
+        {
+           addressShip_s  =   User.addressShips.ToList();
+        }
+    }
+}
