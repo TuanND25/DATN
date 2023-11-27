7743805fc5cbeda11b24016829be4ab97be3c0a2
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2023-11-27 17:16:25 +0700

    len

--- DATN_Client/Areas/Customer/Component/BilllItemByUser.razor.cs
+++ DATN_Client/Areas/Customer/Component/BilllItemByUser.razor.cs
@@ -26,10 +26,43 @@ namespace DATN_Client.Areas.Customer.Component
             _lstBillItems = await _httpClient.GetFromJsonAsync<List<BillDetailShow>>($"https://localhost:7141/api/BillItem/getbilldetail/{_bill.Id}");
 
 
-            var c = Guid.Parse(_ihttpcontextaccessor.HttpContext.Session.GetString("UserId"));
+            //var c = Guid.Parse(_ihttpcontextaccessor.HttpContext.Session.GetString("UserId"));
+            var c = Guid.Parse("F3C0CEA4-8F18-4990-908A-5DF1169F87A2");
             var d = await _httpClient.GetFromJsonAsync<List<AddressShip_VM>>($"https://localhost:7141/api/AddressShip/get_address_by_UserID/{c}");
             _lstAddressGetById = d.OrderByDescending(x => x.Status).ToList();
 
+        }
+        public async Task ChonDiaChiTuList(AddressShip_VM addressShip_VM)
+        {
+            _bill_ShowModel.Recipient = addressShip_VM.Recipient;
+            _bill_ShowModel.PhoneNumber = addressShip_VM.NumberPhone;
+            _bill_ShowModel.Province = addressShip_VM.Province;
+            _bill_ShowModel.District = addressShip_VM.District;
+            _bill_ShowModel.WardName = addressShip_VM.WardName;
+
+            var a = await _httpClient.PutAsJsonAsync("https://localhost:7141/api/Bill/Put-Bill", _bill_ShowModel);
+            if (a.IsSuccessStatusCode)
+            {
+                _toastService.ShowSuccess("Thay đổi địa chỉ thành công");
+            }
+            else
+            {
+                _toastService.ShowError("Thay đổi địa chỉ thất bại");
+            }
+
+        }
+        public async Task HuyDonHang()
+        {
+            _bill_ShowModel.Status = 0;
+            var a = await _httpClient.PutAsJsonAsync("https://localhost:7141/api/Bill/Put-Bill", _bill_ShowModel);
+            if (a.IsSuccessStatusCode)
+            {
+                _toastService.ShowSuccess("Huỷ đơn hàng thành công");
+            }
+            else
+            {
+                _toastService.ShowError("Huỷ đơn hàng thất bại");
+            }
         }
     }
 }
