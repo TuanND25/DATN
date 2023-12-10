b4e85916f1ef8f1b71cbe6db2ffe9a0a5cc0b834
nguyenkien.contact.working@gmail.com <nguyenkien.contact.working@gmail.com>
2023-12-10 15:57:45 +0700

    point

--- DATN_API/Service_IService/Services/SignUpServices.cs
+++ DATN_API/Service_IService/Services/SignUpServices.cs
@@ -155,7 +155,7 @@ namespace DATN_API.Service_IService.Services
                 ConsumerPoint consumerPoint = new ConsumerPoint()
                 { 
                     UserID= id,
-                    Point =string.Empty,
+                    Point ="0",
                     Status= 1,
 
                 };
