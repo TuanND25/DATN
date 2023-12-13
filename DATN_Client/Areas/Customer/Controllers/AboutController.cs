8aced6f9032a4dbc23580908a723e24a52e29290
nguyenkien.contact.working@gmail.com <nguyenkien.contact.working@gmail.com>
2023-12-14 00:40:45 +0700

    about

---
+++ DATN_Client/Areas/Customer/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_Client.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class AboutController : Controller
+	{
+		[Route("about")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
