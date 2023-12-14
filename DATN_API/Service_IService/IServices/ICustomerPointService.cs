06dedef3347c3589784e93b0101553f0a70e8d1b
TuanND25 <nduy.tuan3110@gmail.com>
2023-12-15 01:11:28 +0700

    tuan

--- DATN_API/Service_IService/IServices/ICustomerPointService.cs
+++ DATN_API/Service_IService/IServices/ICustomerPointService.cs
@@ -6,6 +6,7 @@ namespace DATN_API.Service_IService.IServices
     public interface ICustomerPointService
     {
         public Task<List<CustomerPoint_VM>> GetAllCustomerPoint();
+        public Task<CustomerPoint_VM> GetCustomerPoint_byUserID(Guid UserID);
         public Task<string> PutCustomerPoint(CustomerPoint_VM customerPoint_VM);
     }
 }
